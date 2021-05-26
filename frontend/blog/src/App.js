d00c8e5f820ea22623155d711a71eb212c032b79
ulrichschulzendorf <ulrich.s.schulzendorf@gmail.com>
2021-05-26 11:10:09 +0200

    Js frontendadded

--- frontend/blog/src/App.js
+++ frontend/blog/src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Message from "./components/Message";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
+import PostForm from "./components/PostForm";
+import NotFound from "./components/NotFound";
 
-function App() {
+import "./App.css";
+
+const App = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [message, setMessage] = useState(null);
+
+  const setFlashMessage = (message) => {
+    setMessage(message);
+    setTimeout(() => {
+      setMessage(null);
+    }, 1600);
+  };
+
+  const getNewSlugFromTitle = (title) =>
+    encodeURIComponent(title.toLowerCase().split(" ").join("-"));
+
+  const addNewPost = (post) => {
+    post.id = posts.length + 1;
+    post.slug = getNewSlugFromTitle(post.title);
+    setPosts([...posts, post]);
+    setFlashMessage(`saved`);
+  };
+
+  const updatePost = (post) => {
+    post.slug = getNewSlugFromTitle(post.title);
+    const index = posts.findIndex((p) => p.id === post.id);
+    const oldPosts = posts.slice(0, index).concat(posts.slice(index + 1));
+    const updatedPosts = [...oldPosts, post].sort((a, b) => a.id - b.id);
+    setPosts(updatedPosts);
+    setFlashMessage(`updated`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        {message && <Message type={message} />}
+        <Switch>
+          <Route exact path="/" render={() => <Posts posts={posts} />} />
+          <Route
+            path="/post/:postSlug"
+            render={(props) => {
+              const post = posts.find(
+                (post) => post.slug === props.match.params.postSlug
+              );
+              if (post) {
+                return <Post post={post} />;
+              } else {
+                return <Redirect to="/" />;
+              }
+            }}
+          />
+          <Route
+            exact
+            path="/new"
+            render={() => (
+              <PostForm
+                addNewPost={addNewPost}
+                post={{ id: 0, slug: "", title: "", content: "" }}
+              />
+            )}
+          />
+          <Route
+            path="/edit/:postSlug"
+            render={(props) => {
+              const post = posts.find(
+                (post) => post.slug === props.match.params.postSlug
+              );
+              if (post) {
+                return <PostForm updatePost={updatePost} post={post} />;
+              } else {
+                return <Redirect to="/" />;
+              }
+            }}
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
